969214035a20747dd8f3bb12913fbcebaa638dd4
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2023-05-20 19:27:20 +0530

    inital commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import { theme } from "@/themes/theme";
+import { ChakraProvider } from "@chakra-ui/react";
+import { CacheProvider } from "@chakra-ui/next-js";
+import { Inter } from "next/font/google";
+const inter = Inter({
+  subsets: ["latin"],
+});
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-rubik: ${inter.style.fontFamily};
+          }
+        `}
+      </style>
+      <CacheProvider>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </CacheProvider>
+    </>
+  );
 }
